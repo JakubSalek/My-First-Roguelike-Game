10555bcd8f3ac46b6f1175ca22e302cac2711d19
Jakub Sałek <jsalek02@gmail.com>
2022-11-03 11:07:47 +0100

    Work in progress

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import tcod
+import pygame as pg
+from sys import exit
 
-def main():
-    print("Hello World!")
+pg.init()
+screen = pg.display.set_mode((800, 400))
+pg.display.set_caption("University")
+clock = pg.time.Clock()
 
-
-if __name__ == "__main__":
-    main()
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+    pg.display.update()
+    clock.tick(60)
